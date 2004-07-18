fa298d9e770183dbfce9d128ee17a23406728260
PrimusLV <K.Drivnieks@inbox.lv>
2004-07-19 01:46:26 +0300

    Fallback economy class.

---
+++ src/factions/integrations/DummyEconomy.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: primus
+ * Date: 7/19/04
+ * Time: 1:38 AM
+ */
+
+namespace factions\integrations;
+
+
+class DummyEconomy {
+
+    public function __construct(){}
+
+    public function getName() : STRING { return "Dummy"; }
+
+}
